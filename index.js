01fb5d884d15f730e43e54e7b9f6b6a4eee558bc
TolyaKravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2020-05-02 15:53:23 +0300

    add new opportunity to UI,connect to service travelpayouts

--- index.js
+++ index.js
@@ -1,40 +1,102 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1140883126:AAGSkMlkUeLasZ_-NNN8ky52IIYQMsZZja0';
+const TelegramToken = '1140883126:AAGSkMlkUeLasZ_-NNN8ky52IIYQMsZZja0';
+const travelPayOutsToken = 'da8dc06edc902027201164d412a9947e';
+const baseURL = 'http://api.travelpayouts.com/'
 const debug = require('./debug');
+const axios = require('axios');
 
 console.log("Bot has been started");
 
-const bot = new TelegramBot(token, {
+const bot = new TelegramBot(TelegramToken, {
     polling: true
 });
 
-bot.on('message', msg => {
+bot.onText(/\/start/, msg => {
 
     const chatId = msg.chat.id;
 
     bot.sendMessage(chatId, 'Клавиатура', {
         reply_markup: {
             keyboard: [
-                [{
-                    text: 'Узнать моё месторасположение',
-                    request_location: true
-                }],
+                ['Cписок рейсов'],
+                ['Мои забронированные рейсы'],
+                ['Моя информация'],
                 ['Закрыть'],
-                [{
-                    text: 'Узнать мой контакт',
-                    request_contact: true
-                }],
+
             ]
         }
     });
-    if (msg.text === 'Закрыть') {
+})
 
-        bot.sendMessage(chatId, 'Закрываю клавиатуру', {
-            reply_markup: {
-                remove_keyboard: true
-            }
-        })
+bot.on('message', msg => {
 
+    const chatId = msg.chat.id;
+
+    switch (msg.text) {
+        case 'Закрыть':
+            bot.sendMessage(chatId, 'Закрываю клавиатуру', {
+                reply_markup: {
+                    remove_keyboard: true
+                }
+            })
+            break;
+        case 'Моя информация':
+            bot.sendMessage(chatId, 'Получаю информацию обо мне', {
+                reply_markup: {
+                    keyboard: [
+                        [{
+                            text: 'Узнать моё месторасположение',
+                            request_location: true
+                        }],
+                        ['Закрыть'],
+                        [{
+                            text: 'Узнать мой контакт',
+                            request_contact: true
+                        }],
+                    ]
+                }
+            })
+            break;
+        case 'Cписок рейсов':
+            bot.sendMessage(chatId, 'Открываю список рейсов', {
+                reply_markup: {
+                    keyboard: [
+                        ['Ввести город отправления'],
+                    ]
+                }
+            })
+            break;
+        case 'Ввести город отправления':
+            bot.sendMessage(chatId, 'Введите город отправления')
+            let departureCity;
+            let allCity = [];
+            let IATACityCode = [];
+            axios.get(`${baseURL}data/ru/cities.json`)
+                .then(response => {
+                    response.data.map((element) => {
+                        allCity.push(element["name"]);
+                        IATACityCode.push(element["code"])
+                    }).then(
+                        bot.on('message', msg => {
+                            departureCity = allCity.find(item => item === msg.text)
+                            departureCity ? bot.sendMessage(chatId, "Город записан") : bot.sendMessage(chatId, "Город не найден");
+
+                        })
+                    )
+                    bot.sendMessage(chatId, '...', {
+                        reply_markup: {
+                            keyboard: [
+                                ['Найти рейсы в заданном городе'],
+                            ]
+                        }
+                    })
+
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            break;
+        case "Найти рейсы в заданном городе":
     }
 
 });
