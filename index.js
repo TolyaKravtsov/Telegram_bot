3fbb575a42d09d260becebfbf0e91f9ee79bdd59
TolyaKravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2020-05-14 06:03:08 +0300

    add new field to choose flight, add connect to BD

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const TelegramBot = require('node-telegram-bot-api');
 const TelegramToken = '1140883126:AAGSkMlkUeLasZ_-NNN8ky52IIYQMsZZja0';
 const travelPayOutsToken = 'da8dc06edc902027201164d412a9947e';
 const baseURL = 'http://api.travelpayouts.com/'
+const MongoClient = require("mongodb").MongoClient;
 const debug = require('./debug');
 const axios = require('axios');
 let CityObject = {};
 let FindedCity;
+let FindedCityFromTown;
 
+
+const mongoClient = new MongoClient("mongodb://localhost:27017/", {useNewUrlParser: true, useUnifiedTopology: true});
 console.log("Bot has been started");
 
 const bot = new TelegramBot(TelegramToken, {
@@ -20,6 +24,7 @@ bot.onText(/\/start/, msg => {
     bot.sendMessage(chatId, 'Клавиатура', {
         reply_markup: {
             keyboard: [
+
                 ['Cписок рейсов'],
                 ['Мои забронированные рейсы'],
                 ['Моя информация'],
@@ -95,7 +100,7 @@ bot.on('message', msg => {
                                 bot.sendMessage(chatId, '...', {
                                     reply_markup: {
                                         keyboard: [
-                                            ['Найти рейсы в заданном городе'],
+                                            ['Популярные рейсы из города'],
                                         ]
                                     }
                                 })
@@ -105,19 +110,61 @@ bot.on('message', msg => {
                     console.log(error);
                 })
             break;
-        case "Найти рейсы в заданном городе":
+        case "Популярные рейсы из города":
             axios.get(`${baseURL}v1/city-directions?origin=${FindedCity}&currency=usd&token=${travelPayOutsToken}`)
                 .then(response => {
                         Object.keys(response.data.data).map((element) => {
                             element = FindedCity;
                         })
-                        let data = JSON.stringify(response.data.data, null, '\t');
-                        bot.sendMessage(chatId, data);
-                        console.log(data);
+                        FindedCityFromTown = JSON.stringify(response.data.data, null, '\t');
+                        bot.sendMessage(chatId, FindedCityFromTown);
+                        console.log(FindedCityFromTown);
+                        bot.sendMessage(chatId, 'Выбрать рейс', {
+                            reply_markup: {
+                                keyboard: [
+                                    ['Выбрать интересующий вас рейс'],
+                                    ['Закрыть'],
+
+                                ]
+                            }
+                        });
                     }
                 ).catch(error => {
                 console.log(error);
             })
+            break;
+
+        case "Выбрать интересующий вас рейс":
+            bot.sendMessage(chatId, 'Введите краткое название направления(3 буквы)')
+
+            bot.on('message', msg => {
+                let number = msg.text;
+                let ParseTown = JSON.parse(FindedCityFromTown);
+
+                Object.keys(ParseTown).map((el) => {
+                    if (el === number) {
+                        mongoClient.connect((err, client) => {
+                            if (err) {
+                                return console.log(err);
+                            }
+                            const db = client.db("Airport");
+                            const collection = db.collection("flight");
+                            collection.insertOne(ParseTown[el], (err, result) => {
+                                if (err) {
+                                    return console.log(err);
+                                }
+                                console.log(result);
+                                bot.sendMessage(chatId, ParseTown);
+                                console.log("успешное добавление")
+                            })
+
+                        })
+                    }
+                })
+                console.log(FindedCityFromTown);
+            })
+
+            break;
     }
 
 });
